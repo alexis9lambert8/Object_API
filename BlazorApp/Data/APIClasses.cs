fc41f9cef1f8930f9a61030746777d925d31be9f
alexis9lambert8 <40307756+alexis9lambert8@users.noreply.github.com>
2021-05-10 20:49:44 -0400

    init-2

---
+++ BlazorApp/Data/APIClasses.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectsApi.Models
+{
+    public class BaseObject
+    {
+        public string Name;
+        public string Type;
+        public string Id;
+        public List<string> ConnectedEquipment;
+        public bool Active;
+        public Guid AlarmId;
+        public Alarm Alarm;
+    }
+
+    public class Conveyer: BaseObject
+    {
+        public Conveyer(string name, string id, List<string> connectedEquipment, bool active, Guid alarmId, Alarm alarm)
+        {
+            Name = name;
+            Type = "Conveyer";
+            Id = id;
+            ConnectedEquipment = connectedEquipment;
+            Active = active;
+            AlarmId = alarmId;
+            Alarm = alarm;
+        }
+    }
+
+    public class Tug : BaseObject
+    {
+        public Tug(string name, string id, List<string> connectedEquipment, bool active, Guid alarmId, Alarm alarm)
+        {
+            Name = name;
+            Type = "Tug";
+            Id = id;
+            ConnectedEquipment = connectedEquipment;
+            Active = active;
+            AlarmId = alarmId;
+            Alarm = alarm;
+        }
+    }
+
+    public class Tank : BaseObject
+    {
+        public Tank(string name, string id, List<string> connectedEquipment, bool active, Guid alarmId, Alarm alarm)
+        {
+            Name = name;
+            Type = "Tank";
+            Id = id;
+            ConnectedEquipment = connectedEquipment;
+            Active = active;
+            AlarmId = alarmId;
+            Alarm = alarm;
+        }
+    }
+
+    public class Door : BaseObject
+    {
+        public Door(string name, string id, List<string> connectedEquipment, bool active, Guid alarmId, Alarm alarm)
+        {
+            Name = name;
+            Type = "Door";
+            Id = id;
+            ConnectedEquipment = connectedEquipment;
+            Active = active;
+            AlarmId = alarmId;
+            Alarm = alarm;
+        }
+    }
+
+    public class Alarm : BaseObject
+    {
+        public List<string> AffectedEquipment;
+        public int AlarmType;
+        public DateTime TimeOfFailure;
+
+
+        public enum AlarmTypes
+        {
+            Generic = 1
+        }
+        public Alarm(string name, string id, List<string> affectedEquipment, bool active, int alarmType, DateTime timeOfFailure)
+        {
+            Name = name;
+            Type = "Alarm";
+            Id = id;
+            AffectedEquipment = affectedEquipment;
+            Active = active;
+            AlarmType = alarmType;
+            TimeOfFailure = timeOfFailure;
+        }
+        public Alarm()
+        {
+        }
+    }
+
+    public static class ObjectDatabaseSettings
+    {
+        public static string AlarmCollectionName { get; set; } = "Object_API";
+        public static string ConnectionString { get; set; } = "mongodb+srv://defaultUser:9pickles8@cluster0.kgp0x.mongodb.net/Object_API?w=majority";
+        public static string DatabaseName { get; set; } = "Object_API";
+    }
+
+}
---
+++ BlazorApp/Data/Services.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using ObjectsApi.Models;
+
+namespace ObjectsApi.Services
+{
+    public class AlarmService
+    {
+        public IMongoCollection<Alarm> _alarms;
+
+        public AlarmService()
+        {
+            var client = new MongoClient(ObjectDatabaseSettings.ConnectionString);
+            var database = client.GetDatabase(ObjectDatabaseSettings.DatabaseName);
+
+            Console.WriteLine("The list of databases on this server is: ");
+            Console.WriteLine(database);
+
+            _alarms = database.GetCollection<Alarm>(ObjectDatabaseSettings.AlarmCollectionName);
+
+            var collection = database.GetCollection<BaseObject>(ObjectDatabaseSettings.AlarmCollectionName);
+            _alarms = collection.OfType<Alarm>();
+        }
+
+        public List<Alarm> Get() =>
+            _alarms.Find(obj => obj.Type == "Alarm").ToList();
+
+        public Alarm Get(string id) =>
+            _alarms.Find<Alarm>(alarm => alarm.Id == id).FirstOrDefault();
+
+        public Alarm Create(Alarm alarm)
+        {
+            _alarms.InsertOne(alarm);
+            return alarm;
+        }
+
+        public void Update(string id, Alarm targetAlarm) =>
+            _alarms.ReplaceOne(alarm => alarm.Id == id, targetAlarm);
+
+        public void Remove(Alarm targetAlarm) =>
+            _alarms.DeleteOne(alarm => alarm.Id == targetAlarm.Id);
+
+        public void Remove(string id) =>
+            _alarms.DeleteOne(alarm => alarm.Id == id);
+    }
+}
